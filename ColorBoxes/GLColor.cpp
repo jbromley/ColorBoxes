dce5a56137b955adb6883fc1bfc434b16f3302cd
Jay Bromley <jbromley@gmail.com>
2012-12-28 18:43:28 -0800

    Added a class to handle OpenGL colors.

---
+++ ColorBoxes/GLColor.cpp
@@ -0,0 +1,9 @@
+//
+//  GLColor.cpp
+//  ColorBoxes
+//
+//  Created by Jay Bromley on 12/28/12.
+//  Copyright (c) 2012 Jay Bromley. All rights reserved.
+//
+
+#include "GLColor.h"
---
+++ ColorBoxes/GLColor.h
@@ -0,0 +1,14 @@
+//
+//  GLColor.h
+//  ColorBoxes
+//
+//  Created by Jay Bromley on 12/28/12.
+//  Copyright (c) 2012 Jay Bromley. All rights reserved.
+//
+
+#ifndef __ColorBoxes__GLColor__
+#define __ColorBoxes__GLColor__
+
+#include <iostream>
+
+#endif /* defined(__ColorBoxes__GLColor__) */
